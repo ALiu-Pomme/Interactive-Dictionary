688e256c040ce252503b43919cddbdcba092c17e
Amanda Liu <aliu0131@gmail.com>
2018-07-30 22:14:30 -0400

    Fleshed out translate
    It includes leeway for typos and prevents invalid inputs when confirming those typos.

--- app1.py
+++ app1.py
@@ -1,12 +1,22 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word):
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys(), cutoff = 0.75)) > 0:
+        while 1:
+            correction = input("Did you mean %s instead? Please enter Y for yes or N for no: " %get_close_matches(word, data.keys())[0])
+            if correction.lower() == "y":
+                return data[get_close_matches(word, data.keys())[0]]
+            elif correction.lower() == "n":
+                return "The word does not exist, please double check it."
+            else:
+                print("That is not a valid input. Please try again.")
     else:
-        return "The word does not exist, please double check it"
+        return "The word does not exist, please double check it."
 
 word = input("Please enter the word you want to look up: ").lower()
 
