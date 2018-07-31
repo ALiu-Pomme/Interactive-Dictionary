d62f77acc14f3804904cf029a5476cae81441d20
Amanda Liu <aliu0131@gmail.com>
2018-07-31 14:07:49 -0400

    Added acronym conditional
    Returns the definition for USA if something like usa is inputted.

--- app1.py
+++ app1.py
@@ -8,6 +8,8 @@ def translate(word):
         return data[word]
     elif word.title() in data:
         return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys(), cutoff = 0.8)) > 0:
         while 1:
             correction = input("Did you mean %s instead? Please enter Y for yes or N for no: " %get_close_matches(word, data.keys())[0])
